8f82a78abce4cd0f602055cb9eca23c5c590b90a
q-riku <q-riku@users.noreply.github.com>
2021-01-14 13:40:25 +0900

    website

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(function() {
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 60) {
+            $(".header").addClass("scroll");
+            $(".detail").click(function(){
+                $(".header").addClass("click");
+            });
+        } else {
+            $(".header").removeClass("scroll click");
+            $(".detail").click(function(){
+                $(".header").removeClass("click");
+            });
+        }
+        $(".header").removeClass("click");
+    });
+});
+
+$(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 800) {
+            $(".sp-menu").css("display","none");
+        } else {
+            $(".sp-menu").css("display","flex");
+        }
+    });
+});
+
+$(function(){
+    $("#select_1").click(function(){
+        $(this).parent().addClass("active");
+        $("#select_2").parent().removeClass("active");
+        $(".location").css("width","270px");
+        $(".checkin,.checkout,.person").css("display","block");
+        $(".date").css("display","none");
+    });
+});
+
+$(function(){
+    $("#select_2").click(function(){
+        $(this).parent().addClass("active");
+        $("#select_1").parent().removeClass("active");
+        $(".date").css("display","block");
+        $(".location").css("width","410px");
+        $(".checkin,.checkout,.person").css("display","none");
+    });
+});
+
+$(function () {
+    $("#btn").click(function () {
+        $(".menu").toggleClass("click");
+    });
+});
+
